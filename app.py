884a4c6811d6b322ba6bdbf00fb8cb41ea40e331
Moonmoo <ansaudwn1234@gmail.com>
2022-05-19 16:08:27 +0900

    index.html / 파일업로드 및 리스팅 app.py / 파일업로드 및 리스팅

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from datetime import datetime
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
-import json
+
 
 app = Flask(__name__)
 
